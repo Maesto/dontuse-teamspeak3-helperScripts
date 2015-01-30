e040e0d703bffe34181115555a85b4ebe6886d5e
Lucas Wendel <maesto@cccmz.de>
2015-01-30 01:24:30 +0100

    implemented auto update

--- opt/update.sh
+++ opt/update.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 
+if [ "$(id -u)" == "0" ]; then
+   echo "This script cant be run  as root" 1>&2
+   exit 2
+fi
+
+tsScriptVar="../var"
+tsScriptDir=${0%/*}
+cd "$tsScriptDir"
+
+./getCurrentTarball.sh
+
+if( [[ $? -eq 1 ]] )
+then
+	echo "Update Needed"
+else
+	return 0
+fi
+
+$($stopCMD)
+lastVer=$(cat "$tsScriptVar"/version)
+tar -xf "$tsScriptVar"/"$lastVer" --strip-components=1 -C "$tsdir"
+$($startCMD)
